1130cb2d12e8ae0848953771e4bc554aa7880d33
Juan Pablo Gaviria <jjgaviriaa@eafit.edu.co>
2025-08-20 23:35:19 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
-# Create your views here.
 def home(request):
-    #return HttpResponse('<h1>Welcome to Movie Reviews</h1><p>This is the home page.<h1>')
-    #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Juan Pablo Gaviria'})
+    # Obtener lo que el usuario escribió en la barra de búsqueda
+    searchTerm = request.GET.get('searchMovie', '')
+
+    # Si hay término de búsqueda, filtrar por título
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    # Renderizar la plantilla con los datos
+    return render(request, 'home.html', {
+        'searchTerm': searchTerm,
+        'movies': movies
+    })
+
+
 def about(request):
-    return render(request,'about.html') 
+    return HttpResponse('<h1>Welcome to About Page</h1>')
